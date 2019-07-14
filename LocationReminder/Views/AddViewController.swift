b7842ffbded8654c620f3fbca58ec20b131b5244
master <>
2019-07-15 02:29:18 +0900

    add viewcontroller

---
+++ LocationReminder/Views/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  LocationReminder
+//
+//  Created by 조전형 on 2019/07/15.
+//  Copyright © 2019 cho. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
